ebef9677230d1c5172bb9bccae8ab1d66fac07d7
pwathome <pwathome@biscuits.local>
2023-09-26 21:37:46 -0400

    organize and format

--- code/M06_saving_and_reading_data.py
+++ code/M06_saving_and_reading_data.py
@@ -3,6 +3,7 @@ import os.path
 
 # variable to check if file exists
 mood_file = os.path.isfile("./how_was_your_day.txt")
+# ask_question function
 def ask_question():
     print("Welcome to mood checker!")
     while True:
@@ -11,6 +12,11 @@ def ask_question():
         if question in ["good", "bad", ""]:
             # initialize todays time & date
             today = datetime.now().ctime()
+            # ternary operator to print message. yes I know it's ugly lol
+            if question == "bad":
+                print("Cheer up! Tomorrow is another day 😀")
+            else:
+                print("That's fantastic to hear 🐵")
             # write answer to a file
             with open("how_was_your_day.txt", "w") as file:
                 file.write(f"{today}\n")
@@ -21,10 +27,7 @@ def ask_question():
             print("Enter 'good' or 'bad' please!")
 
 
-# check for existing file first
-if mood_file == False:
-    ask_question()
-else:
+def check_mood():
     # initialize empty file_date array
     file_data = []
     # open the file and loop through lines
@@ -44,4 +47,17 @@ else:
         ask_question()
     else:
         print("Okay have a fantastic day!")
+    
+
+def main():
+    # check for existing file first
+    if mood_file == False:
+        ask_question()
+    else:
+        check_mood()
+   
+
+# runs the program
+if __name__ == "__main__":
+    main()
 
