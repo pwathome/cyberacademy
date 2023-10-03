bfac530b3c1d08bc7a026230ae3042f21d83264b
pwathome <pwathome@biscuits.local>
2023-10-02 20:47:46 -0400

    start working on get_money function

---
+++ code/scratchpad.py
@@ -0,0 +1,38 @@
+# get money function
+def get_money():
+    money_types = {"hundreds": {
+                            "key": "a",
+                            "value": 100},
+                    "fifties": {
+                            "key": "b",
+                            "value": 50},
+                    "twenties": {
+                            "key": "c",
+                            "value": 20},
+                    "tens": {
+                            "key": "d",
+                            "value": 10},
+                    "one's": {
+                            "key": "e",
+                            "value": 1}}
+    
+    monies = []
+    # print(money_types["hundreds"])
+    # print(money_types.keys())
+    # if question in ["a","b","c","d","e"]:
+    for money in money_types.keys():
+        # hard coded choice for now
+        question = "hundreds"
+        money_choice = money_types[question]
+        # print("Which money type?:",money_choice)
+        if question == money:
+            print("Working with:",money)
+            print("Worth:",money_types[money]["value"])
+            how_many = 2
+            total = how_many * money_types[money]["value"]
+            print(f"Total for {money}'s: {total}")
+            return total
+
+        return money
+
+get_money()
