ab32072354a5c0e5afab0321a1e0cad7eca16dee
pwathome <pwathome@biscuits.local>
2023-10-04 13:36:15 -0400

    making a more secure hashed pw

--- code/26_login_program/7_getting_salty.py
+++ code/26_login_program/7_getting_salty.py
@@ -1,19 +1,31 @@
-# from hashlib import md5, sha256
-# from getpass import getpass
+from hashlib import md5, sha512
+from getpass import getpass as gp
 
 
 # def old_way(prompt="Enter your password: "):
-#     passwd = sha256(getpass(prompt).encode("UTF-8")).hexdigest()
+#     passwd = sha512(getpass(prompt).encode("UTF-8")).hexdigest()
 #     return passwd
 
 
 # print(old_way())
 
 # def salted(prompt="Enter your password: "):
-#     return sha256("".join(
+#     return sha512("".join(
 #         ("SaltThisHash!", getpass(prompt), "NoPwn4U")
 #         ).encode("UTF-8")).hexdigest()
 
 
 # print(salted())
+
+def passwd(username, prompt="Enter your password: "):
+    salt = md5(username.encode("UTF-8")).hexdigest()
+    return sha512("".join(
+        ("SaltTheHasNotPwn4u", gp(prompt), salt)
+        ).encode("UTF-8")).hexdigest()
+
+
+user1 = input("Enter user1 name: ")
+user2 = input("Enter user2 name: ")
+print(passwd(user1))
+print(passwd(user2))
 
